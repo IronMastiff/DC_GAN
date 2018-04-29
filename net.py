eebaad0b8a7006e743b900ac7a83de84edda704b
Iron Mastiff <1760298283@qq.com>
2018-04-29 18:57:10 +0800

    Finish

--- net.py
+++ net.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-def model_input( real_dim, z_dim ):
+def model_inputs( real_dim, z_dim ):
      inputs_real = tf.placeholder( tf.float32, ( None, *real_dim ), name = 'input_real' )
      inputs_z = tf.placeholder( tf.float32, ( None, z_dim ), name = 'input_z' )
 
@@ -25,7 +25,7 @@ def generator( z, output_dim, reuse = False, alpha = 0.2, training = True ):
         # 8 * 8 * 256
 
         x3 = tf.layers.conv2d_transpose( x2, 128, 5, strides = 2, padding = 'same' )
-        x3 = tf.layers.batch_normalization( x3, training = trianing )
+        x3 = tf.layers.batch_normalization( x3, training = training )
         x3 = tf.maximum( x3, alpha * x3 )
         # 16 * 16 * 128
 
@@ -44,16 +44,16 @@ def discriminator( x, reuse = False, alpha = 0.2 ):
 
         x2 = tf.layers.conv2d( relu1, 128, 5, strides = 2, padding = 'same' )
         bn2 = tf.layers.batch_normalization( x2, training = True )
-        relu2 = tf.maximunj( bn2, alpha * bn2 )
+        relu2 = tf.maximum( bn2, alpha * bn2 )
         # 8 * 8 * 128
 
         x3 = tf.layers.conv2d( relu2, 256, 5, strides = 2, padding = 'same' )
-        bn3 = tf.layers.batch_normalization( x3, training = x3 )
-        relu3 = tf.maximun( bn3, bn3 * alpha )
+        bn3 = tf.layers.batch_normalization( x3, training = True )
+        relu3 = tf.maximum( bn3, bn3 * alpha )
         # 4 * 4 * 256
 
         # Flatten it
-        flat = tf.reshape( relu3, ( -1, relu3 ) )
+        flat = tf.reshape( relu3, ( -1, 4 * 4 * 256 ) )
         logits = tf.layers.dense( flat, 1 )
         out = tf.sigmoid( logits )
 
@@ -98,18 +98,18 @@ def model_opt( d_loss, g_loss, learning_rate, beta1 ):
     """
     # Get weight and bias to undate
     t_vars = tf.trainable_variables()
-    d_vars = [var for var in t_vars if var.name.startwith( 'discriminator' )]
-    g_vars = [var for var in t_vars if var.name.startwith( 'generator' )]
+    d_vars = [var for var in t_vars if var.name.startswith( 'discriminator' )]
+    g_vars = [var for var in t_vars if var.name.startswith( 'generator' )]
 
     # Optimize
     with tf.control_dependencies( tf.get_collection( tf.GraphKeys.UPDATE_OPS ) ):
-        d_train_opt = tf.train.AdamOptimizer( learning_rate, beta1 = beta1 ).minimize( d_loss, val_list = d_vals )
-        g_train_opt = tf.train.AdamOptimizer( learning_rate, beta1 = beta1 ).minimize( g_loss, var_list = g_vals )
+        d_train_opt = tf.train.AdamOptimizer( learning_rate, beta1 = beta1 ).minimize( d_loss, var_list = d_vars )
+        g_train_opt = tf.train.AdamOptimizer( learning_rate, beta1 = beta1 ).minimize( g_loss, var_list = g_vars )
 
     return d_train_opt, g_train_opt
 
 class GAN:
-    def __init__( self, real_size, z_size, learning_rate, aplha = 0.2, beta1 = 0.5 ):
+    def __init__( self, real_size, z_size, learning_rate, alpha = 0.2, beta1 = 0.5 ):
         tf.reset_default_graph()
 
         self.input_real, self.input_z = model_inputs( real_size, z_size )
