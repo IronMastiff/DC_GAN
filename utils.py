aa51d16ae45e3cec6faee184b28e194d484abdfa
Iron Mastiff <1760298283@qq.com>
2018-04-30 13:44:23 +0800

    Finish

--- utils.py
+++ utils.py
@@ -13,10 +13,10 @@ def read_conf_file(conf_file):
     return FLAGS
 
 def view_samples( epoch, samples, nrows, ncols, figsize = ( 5, 5 ) ):
-    fig, axes = plt.subplots( figszie = figsize, nrows = nrows, ncols = ncols, sharey = True, sharex = True )
+    fig, axes = plt.subplots( figsize = figsize, nrows = nrows, ncols = ncols, sharey = True, sharex = True )
     for ax, img in zip( axes.flatten(), samples[epoch] ):
         ax.axis( 'off' )
-        img = ( ( img - img.min() ) * 255 / ( img.max() - img.min() ) ).astype( np.uit8 )
+        img = ( ( img - img.min() ) * 255 / ( img.max() - img.min() ) ).astype( np.uint8 )
         ax.set_adjustable( 'box-forced' )
         im = ax.imshow( img, aspect = 'equal' )
 
